f091663b6d7916f0d20418f34a6082910ad120b5
Bill <wkernan@gmail.com>
2016-07-22 13:18:39 -0500

    minor front end changes

--- server.js
+++ server.js
@@ -1,9 +1,7 @@
 var express = require('express');
 var bodyParser = require('body-parser');
 var path = require('path');
-
 var app = express();
-var PORT = 3000;
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: true}));
